d8756b740689211a3896d5cf73af0fd96322127a
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-09-15 03:17:23 +0900

    Initial commit

---
+++ Leafy/Leafy/LeafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyApp.swift
+//  Leafy
+//
+//  Created by Jiyoung Park on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct LeafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
